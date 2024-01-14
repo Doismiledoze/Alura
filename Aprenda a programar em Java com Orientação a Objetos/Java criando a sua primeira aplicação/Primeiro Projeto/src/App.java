2d90da05f8de2330639ae716ae460134cc111411
Guilherme Henrique Alves <alves.guilherme.profissional+github@gmail.com>
2024-01-14 16:47:19 -0300

    01. Primeiro programa em Java

---
+++ Aprenda a programar em Java com Orientação a Objetos/Java criando a sua primeira aplicação/Primeiro Projeto/src/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        //System.out.println("Alô, Mundo!");
+        System.out.println("Concluí a aula 01 e agora estou mergulhando em Java!");
+    }
+}
