4da098acff314b04287ed05f3704784ac1e7cfb3
Guilherme Henrique Alves <alves.guilherme.profissional+github@gmail.com>
2024-01-23 21:59:37 -0300

    04. Detalhando o Screenmatch

--- Java Web crie aplicações usando Spring Boot/Java criando sua primeira API e conectando ao front/Screen Match/src/main/java/br/com/alura/screenmatch/controller/SerieController.java
+++ Java Web crie aplicações usando Spring Boot/Java criando sua primeira API e conectando ao front/Screen Match/src/main/java/br/com/alura/screenmatch/controller/SerieController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.alura.screenmatch.dto.EpisodioDTO;
 import br.com.alura.screenmatch.dto.SerieDTO;
 import br.com.alura.screenmatch.service.SerieService;
 
@@ -42,6 +43,21 @@ public class SerieController {
     @GetMapping("/{id}")
     public SerieDTO obterPorId(@PathVariable Long id){
         return servico.obterPorId(id);
+    }
+
+    @GetMapping("/{id}/temporadas/todas")
+    public List<EpisodioDTO> obterTodasTemporadas(@PathVariable Long id){
+        return servico.obterTodasTemporadas(id);
+    }
+
+    @GetMapping("/{id}/temporadas/{numero}")
+    public List<EpisodioDTO> obterTodasTemporadas(@PathVariable Long id, @PathVariable Long numero){
+        return servico.obterTemporadasPorNumero(id,numero);
+    }
+
+    @GetMapping("/categoria/{nomeGenero}")
+    public List<SerieDTO> obterSeriesPorCategoria(@PathVariable String nomeGenero){
+        return servico.obterSeriesPorCategoria(nomeGenero);
     }
 
 }
--- Java Web crie aplicações usando Spring Boot/Java criando sua primeira API e conectando ao front/Screen Match/src/main/java/br/com/alura/screenmatch/service/SerieService.java
+++ Java Web crie aplicações usando Spring Boot/Java criando sua primeira API e conectando ao front/Screen Match/src/main/java/br/com/alura/screenmatch/service/SerieService.java
@@ -7,7 +7,9 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.com.alura.screenmatch.dto.EpisodioDTO;
 import br.com.alura.screenmatch.dto.SerieDTO;
+import br.com.alura.screenmatch.model.Categoria;
 import br.com.alura.screenmatch.model.Serie;
 import br.com.alura.screenmatch.repository.SerieRepository;
 
@@ -19,38 +21,67 @@ public class SerieService {
 
     public List<SerieDTO> obterTodasAsSeries() {
         // return repositorio
-        //         .findAll().stream().map(s -> new SerieDTO(s.getId(), s.getTitulo(), s.getTotalTemporadas(),
-        //                 s.getAvaliacao(), s.getGenero(), s.getAtores(), s.getPoster(), s.getSinopse()))
-        //         .collect(Collectors.toList());
-        return converteDados(repositorio.findAll()); 
+        // .findAll().stream().map(s -> new SerieDTO(s.getId(), s.getTitulo(),
+        // s.getTotalTemporadas(),
+        // s.getAvaliacao(), s.getGenero(), s.getAtores(), s.getPoster(),
+        // s.getSinopse()))
+        // .collect(Collectors.toList());
+        return converteDados(repositorio.findAll());
     }
 
     public List<SerieDTO> obterTop5Series() {
         // return repositorio
-        //         .findTop5ByOrderByAvaliacaoDesc().stream().map(s -> new SerieDTO(s.getId(), s.getTitulo(), s.getTotalTemporadas(),
-        //                 s.getAvaliacao(), s.getGenero(), s.getAtores(), s.getPoster(), s.getSinopse()))
-        //         .collect(Collectors.toList()); 
+        // .findTop5ByOrderByAvaliacaoDesc().stream().map(s -> new SerieDTO(s.getId(),
+        // s.getTitulo(), s.getTotalTemporadas(),
+        // s.getAvaliacao(), s.getGenero(), s.getAtores(), s.getPoster(),
+        // s.getSinopse()))
+        // .collect(Collectors.toList());
         return converteDados(repositorio.findTop5ByOrderByAvaliacaoDesc());
     }
 
     public List<SerieDTO> obterLancamentos() {
-        //return converteDados(repositorio.findTop5ByOrderByEpisodiosDataLancamentoDesc());
+        // return
+        // converteDados(repositorio.findTop5ByOrderByEpisodiosDataLancamentoDesc());
         return converteDados(repositorio.encontrarEpisodiosMaisRecentes());
     }
 
-    private List<SerieDTO> converteDados(List<Serie> series){
+    private List<SerieDTO> converteDados(List<Serie> series) {
         return series.stream().map(s -> new SerieDTO(s.getId(), s.getTitulo(), s.getTotalTemporadas(),
-        s.getAvaliacao(), s.getGenero(), s.getAtores(), s.getPoster(), s.getSinopse()))
-.collect(Collectors.toList()); 
+                s.getAvaliacao(), s.getGenero(), s.getAtores(), s.getPoster(), s.getSinopse()))
+                .collect(Collectors.toList());
     }
 
     public SerieDTO obterPorId(Long id) {
         Optional<Serie> serie = repositorio.findById(id);
-        if(serie.isPresent()){
+        if (serie.isPresent()) {
             Serie s = serie.get();
             return new SerieDTO(s.getId(), s.getTitulo(), s.getTotalTemporadas(),
-            s.getAvaliacao(), s.getGenero(), s.getAtores(), s.getPoster(), s.getSinopse());
+                    s.getAvaliacao(), s.getGenero(), s.getAtores(), s.getPoster(), s.getSinopse());
         }
         return null;
     }
+
+    public List<EpisodioDTO> obterTodasTemporadas(Long id) {
+        Optional<Serie> serie = repositorio.findById(id);
+        if (serie.isPresent()) {
+            Serie s = serie.get();
+            return s.getEpisodios().stream()
+                    .map(e -> new EpisodioDTO(e.getTemporada(), e.getTitulo(), e.getNumeroEpisodio()))
+                    .collect(Collectors.toList());
+        }
+        return null;
+    }
+
+    public List<EpisodioDTO> obterTemporadasPorNumero(Long id, Long numero) {
+        return repositorio.obterEpisodiosPorTemporada(id, numero)
+                        .stream()
+                        .map(e -> new EpisodioDTO(e.getTemporada(), e.getTitulo(), e.getNumeroEpisodio()))
+                        .collect(Collectors.toList());
+}
+
+    public List<SerieDTO> obterSeriesPorCategoria(String nomeGenero) {
+        Categoria categoria = Categoria.fromPortugues(nomeGenero);
+        return converteDados(repositorio.findByGenero(categoria));
+    }
+
 }
