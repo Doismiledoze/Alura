4af05ef97e402bdb774d6ce9b4b90f2e0516c485
Guilherme Alves UD <alves.guilherme.profissional+github@gmail.com>
2024-04-26 15:05:59 -0300

    02. Requisições com Axios

--- Full stack JavaScript crie um projeto com React e Node.js/React e Node.js consumindo APIs no React no projeto full stack/alura-books-server/app.js
+++ Full stack JavaScript crie um projeto com React e Node.js/React e Node.js consumindo APIs no React no projeto full stack/alura-books-server/app.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = 8000;
 const rotaLivro = require("./rotas/livro")
-app.use(express.json())
+app.use(express.json());
+app.use(cors({origin:"*"}))
 app.use('/livros',rotaLivro);
 
 app.listen(port, ()=> console.log(`Escutanto a porta ${port}`));
