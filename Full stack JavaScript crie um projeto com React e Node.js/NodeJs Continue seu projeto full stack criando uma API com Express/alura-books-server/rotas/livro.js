acf4d4fe59a4f3eefcb850ee1939a773bbb5a4e4
Guilherme Alves UD <alves.guilherme.profissional+github@gmail.com>
2024-04-26 11:01:01 -0300

    04. Parâmetros nas requisições

--- Full stack JavaScript crie um projeto com React e Node.js/NodeJs Continue seu projeto full stack criando uma API com Express/alura-books-server/rotas/livro.js
+++ Full stack JavaScript crie um projeto com React e Node.js/NodeJs Continue seu projeto full stack criando uma API com Express/alura-books-server/rotas/livro.js
@@ -1,19 +1,14 @@
 const { Router } = require("express");
-const {getLivros} = require("../controladores/livro");
+const {getLivros, getLivro, postLivro, patchLivro, deleteLivro} = require("../controladores/livro");
 const router = Router();
 
 router.get('/',getLivros)
+router.get('/:id',getLivro)
 
-router.post('/',(req, res)=>{
-    res.send("Você fez uma requisição do tipo POST")
-})
+router.post('/', postLivro)
 
-router.patch('/',(req, res)=>{
-    res.send("Você fez uma requisição do tipo PATCH")
-})
+router.patch('/:id',patchLivro)
 
-router.delete('/',(req, res)=>{
-    res.send("Você fez uma requisição do tipo DELETE")
-})
+router.delete('/:id',deleteLivro)
 
 module.exports = router;
