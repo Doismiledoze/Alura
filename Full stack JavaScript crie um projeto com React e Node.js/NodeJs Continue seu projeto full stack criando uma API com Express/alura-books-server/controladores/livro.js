6bc0e27c516c9af6be55b5d1e59bea6964e98afb
Guilherme Alves UD <alves.guilherme.profissional+github@gmail.com>
2024-04-26 11:37:52 -0300

    05. Lidando com erros

--- Full stack JavaScript crie um projeto com React e Node.js/NodeJs Continue seu projeto full stack criando uma API com Express/alura-books-server/controladores/livro.js
+++ Full stack JavaScript crie um projeto com React e Node.js/NodeJs Continue seu projeto full stack criando uma API com Express/alura-books-server/controladores/livro.js
@@ -13,8 +13,13 @@ function getLivros(req, res) {
 function getLivro(req, res) {
     try {
         const id = req.params.id;
-        const livro = getLivroPorId(id);
-        res.send(livro);
+        if (id && Number(id)) {
+            const livro = getLivroPorId(id);
+            res.send(livro);
+        } else {
+            res.status(422);
+            res.send("ID inválido");
+        }
     } catch (error) {
         res.status(500);
         res.send(error.message);
@@ -23,11 +28,17 @@ function getLivro(req, res) {
 
 function postLivro(req, res) {
     try {
-        const livroNovo = req.body;
-        insereLivro(livroNovo);
-        res.status(201);
-        res.send("Livro inserido com sucesso");
-    } catch (error) {
+        const livroNovo = req.body
+        if(req.body.nome) {
+            insereLivro(livroNovo);
+            res.status(201);
+            res.send("Livro inserido com sucesso");
+        } else {
+            res.status(422);
+            res.send("O campo nome é obrigatório");
+        }
+
+    } catch(error) {
         res.status(500);
         res.send(error.message);
     }
@@ -37,8 +48,13 @@ function patchLivro(req, res) {
     try {
         const id = req.params.id;
         const body = req.body;
-        modificaLivro(body,id);
-        res.send("Item modificado com sucesso");
+        if (id && Number(id)) {
+            modificaLivro(body, id);
+            res.send("Item modificado com sucesso");
+        } else {
+            res.status(422);
+            res.send("ID inválido");
+        }
     } catch (error) {
         res.status(500);
         res.send(error.message);
@@ -48,8 +64,13 @@ function patchLivro(req, res) {
 function deleteLivro(req, res) {
     try {
         const id = req.params.id;
-        deletaLivroPorId(id);
-        res.send("Livro deletado com sucesso");
+        if (id && Number(id)) {
+            deletaLivroPorId(id);
+            res.send("Livro deletado com sucesso");
+        } else {
+            res.status(422);
+            res.send("ID inválido");
+        }
     } catch (error) {
         res.status(500);
         res.send(error.message);
